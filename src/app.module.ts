e438128543a883de958ee24cc5c62affc9520f16
Gabriel Couto <gabrielcouto26@gmail.com>
2025-06-08 20:12:30 -0300

    add redis and rds postgres resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { NotificationModule } from './notification/notification.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { WalletModule } from './wallet/wallet.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './database';
 
 @Module({
-  imports: [NotificationModule, TransactionModule, WalletModule],
+  imports: [
+    TypeOrmModule.forRoot(AppDataSource.options),
+    NotificationModule,
+    TransactionModule,
+    WalletModule,
+  ],
 })
 export class AppModule {}
