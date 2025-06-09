38a343a71032e6266381fd0553ba79dd76d086d8
Gabriel Couto <gabrielcouto26@gmail.com>
2025-06-08 23:04:27 -0300

    init repositories

---
+++ src/database/repositories/wallet.repository.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Wallet } from '@core/wallet/entities/wallet.entity';
+
+@Injectable()
+export default class WalletRepository extends Repository<Wallet> {}
