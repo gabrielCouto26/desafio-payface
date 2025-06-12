2e8d711e37bf09a48c79b33c1a9dc59ecd0d5e21
Gabriel Couto <gabrielcouto26@gmail.com>
2025-06-12 00:27:31 -0300

    fix cors

--- src/app/wallet/wallet.controller.ts
+++ src/app/wallet/wallet.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Header, HttpCode, Post } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { TransactionDto } from '../../core/transaction/dto/transaction.dto';
 import TransactionValidator from '../shared/transaction.validator';
@@ -8,6 +8,10 @@ export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
   @Post('send')
+  @HttpCode(201)
+  @Header('Access-Control-Allow-Origin', '*')
+  @Header('Access-Control-Allow-Headers', 'Content-Type')
+  @Header('Access-Control-Allow-Methods', 'POST')
   async sendMoney(@Body() event: TransactionDto) {
     TransactionValidator.validate(event);
     return this.walletService.sendMoney(event);
